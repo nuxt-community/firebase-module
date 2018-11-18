252b1318b6a060a964c4b319fc46c2195b339858
Pascal <pascal@luther.ch>
2018-11-18 13:33:50 +0100

    renamed options.use to options.useOnly to make it more clear

--- plugin.js
+++ plugin.js
@@ -14,23 +14,23 @@ export default function({ app }, inject) {
   }
 
   let _fireStore, _fireFunc, _fireStorage, _fireAuth
-  if (!options.use || options.use.includes('firestore')) {
+  if (!options.useOnly || options.useOnly.includes('firestore')) {
     firebase.firestore().settings({ timestampsInSnapshots: true })
     _fireStore = firebase.firestore()
     inject('fireStore', _fireStore)
   }
 
-  if (!options.use || options.use.includes('functions')) {
+  if (!options.useOnly || options.useOnly.includes('functions')) {
     _fireFunc = firebase.functions()
     inject('fireFunc', _fireFunc)
   }
 
-  if (!options.use || options.use.includes('storage')) {
+  if (!options.useOnly || options.useOnly.includes('storage')) {
     _fireStorage = firebase.storage()
     inject('fireStorage', _fireStorage)
   }
 
-  if (!options.use || options.use.includes('auth')) {
+  if (!options.useOnly || options.useOnly.includes('auth')) {
     const _fireAuth = firebase.auth()
     inject('fireAuth', _fireAuth)
   }
