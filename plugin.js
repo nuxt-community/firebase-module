bda799a14495edf1c7c6364f698efe2756bb2ede
Pascal <pascal@luther.ch>
2018-11-18 18:18:01 +0100

    injext $ into ctx, cleaned up code a little

--- plugin.js
+++ plugin.js
@@ -5,7 +5,7 @@ import 'firebase/database'
 import 'firebase/storage'
 import 'firebase/auth'
 
-export default function({ app }, inject) {
+export default (ctx, inject) => {
   
   const options = <%= serialize(options) %>
 
@@ -18,32 +18,35 @@ export default function({ app }, inject) {
     }
   }
 
-  let _fireStore, _fireFunc, _fireStorage, _fireAuth, _fireDb
-
   if (!options.useOnly || options.useOnly.includes('firestore')) {
     firebase.firestore().settings({ timestampsInSnapshots: true })
-    _fireStore = firebase.firestore()
-    inject('fireStore', _fireStore)
+    const fireStore = firebase.firestore()
+    ctx.$fireStore = fireStore
+    inject('fireStore', fireStore)
   }
 
   if (!options.useOnly || options.useOnly.includes('realtimeDb')) {
-    _fireDb = firebase.database()
-    inject('fireDb', _fireDb)
+    const fireDb = firebase.database()
+    ctx.$fireDb = fireDb
+    inject('fireDb', fireDb)
   }
 
   if (!options.useOnly || options.useOnly.includes('functions')) {
-    _fireFunc = firebase.functions()
-    inject('fireFunc', _fireFunc)
+    const fireFunc = firebase.functions()
+    ctx.$fireFunc = fireFunc
+    inject('fireFunc', fireFunc)
   }
 
   if (!options.useOnly || options.useOnly.includes('storage')) {
-    _fireStorage = firebase.storage()
-    inject('fireStorage', _fireStorage)
+    const fireStorage = firebase.storage()
+    ctx.$fireStorage = fireStorage
+    inject('fireStorage', fireStorage)
   }
 
   if (!options.useOnly || options.useOnly.includes('auth')) {
-    const _fireAuth = firebase.auth()
-    inject('fireAuth', _fireAuth)
+    const fireAuth = firebase.auth()
+    ctx.$fireAuth = fireAuth
+    inject('fireAuth', fireAuth)
   }
 
 }
