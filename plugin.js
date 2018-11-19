16f1b62ed5f60fdce6360b645b97f0240324a654
Pascal <pascal@luther.ch>
2018-11-19 01:39:20 +0100

    (1) improved conditional import (2) set useOnly default as array not null (3) check also for incomplete devConfig

--- plugin.js
+++ plugin.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/functions'
-import 'firebase/database'
-import 'firebase/storage'
-import 'firebase/auth'
+<%= options.useOnly.includes('firestore') ? "import 'firebase/firestore'" : "" %>
+<%= options.useOnly.includes('functions') ? "import 'firebase/functions'" : "" %>
+<%= options.useOnly.includes('realtimeDb') ? "import 'firebase/database'" : "" %>
+<%= options.useOnly.includes('storage') ? "import 'firebase/storage'" : "" %>
+<%= options.useOnly.includes('auth') ? "import 'firebase/auth'" : "" %>
 
 export default (ctx, inject) => {
   
@@ -18,28 +18,30 @@ export default (ctx, inject) => {
     }
   }
 
-  if (!options.useOnly || options.useOnly.includes('firestore')) {
+  if (options.useOnly.includes('firestore')) {
+    // Next line is needed, but only temporary due to changes in Firebase JS SDK
+    // see https://firebase.google.com/support/release-notes/js  -> Version 4.13.0
     firebase.firestore().settings({ timestampsInSnapshots: true })
     const fireStore = firebase.firestore()
     inject('fireStore', fireStore)
   }
 
-  if (!options.useOnly || options.useOnly.includes('realtimeDb')) {
+  if (options.useOnly.includes('realtimeDb')) {
     const fireDb = firebase.database()
     inject('fireDb', fireDb)
   }
 
-  if (!options.useOnly || options.useOnly.includes('functions')) {
+  if (options.useOnly.includes('functions')) {
     const fireFunc = firebase.functions()
     inject('fireFunc', fireFunc)
   }
 
-  if (!options.useOnly || options.useOnly.includes('storage')) {
+  if (options.useOnly.includes('storage')) {
     const fireStorage = firebase.storage()
     inject('fireStorage', fireStorage)
   }
 
-  if (!options.useOnly || options.useOnly.includes('auth')) {
+  if (options.useOnly.includes('auth')) {
     const fireAuth = firebase.auth()
     inject('fireAuth', fireAuth)
   }
