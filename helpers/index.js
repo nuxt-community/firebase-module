65d35c582f449d14a6fa1c353676edbaaece218c
Pascal <pascal@luther.ch>
2019-11-13 00:25:12 +0800

    applied prettier

--- helpers/index.js
+++ helpers/index.js
@@ -2,8 +2,8 @@ export function movePluginBeforeInitAuthPlugin(plugins, pluginName) {
   // Moves a plugin that needs access to $this.fireFOO but needs to get run BEFORE nuxt-auth in between the nuxt-fire and the auth plugin.
   // This function needs to be applied in extendPlugins in nuxt.config.js.
 
-  const fromIndex = plugins.findIndex(x => x.src.includes(pluginName))
-  const toIndex = plugins.findIndex(x => x.src.includes('nuxt-fire/main')) + 1
+  const fromIndex = plugins.findIndex((x) => x.src.includes(pluginName))
+  const toIndex = plugins.findIndex((x) => x.src.includes('nuxt-fire/main')) + 1
 
   if (fromIndex === -1) {
     const msg = `Nuxt-Fire Helpers: Could not find a plugin that includes the name: ${pluginName}.`
