a0999e381921bd54f578c1f752791c5204d1478c
Pascal <pascal@luther.ch>
2019-12-13 11:24:38 +0800

    added @vuepress/theme-nuxtfire

---
+++ packages/docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top>
+      <!--<SidebarTopAds /> -->
+    </template>
+    <template #page-bottom>
+      <!--<PageBottomAds />-->
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import Vue from 'vue'
+import AutoAdsense from 'vue-google-adsense/dist/InFeedAdsense.min.js'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout: () => import('@parent-theme/layouts/Layout.vue'),
+    SidebarTopAds: () => import('@theme/components/SidebarTopAds')
+  },
+  beforeMount() {
+    Vue.use(require('vue-script2'))
+    Vue.use(AutoAdsense, { adClient: 'ca-pub-6032774483827005' })
+  }
+}
+</script>
