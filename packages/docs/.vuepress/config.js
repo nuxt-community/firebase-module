8c2428499b5b6fd3fbfc94a4a82315120ee77ef1
Pascal <pascal@luther.ch>
2019-12-29 11:47:31 +0800

    added more section

--- packages/docs/.vuepress/config.js
+++ packages/docs/.vuepress/config.js
@@ -48,12 +48,21 @@ module.exports = {
           sidebarDepth: 2, // optional, defaults to 1
           children: ['/tutorials/ssr/', '/tutorials/vuexfire/']
         }
+      ],
+      '/more/': [
+        {
+          title: 'More', // required
+          collapsable: false, // optional, defaults to true
+          sidebarDepth: 1, // optional, defaults to 1
+          children: ['/more/links/']
+        }
       ]
     },
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/introduction/' },
       { text: 'Tutorials', link: '/tutorials/ssr/' },
+      { text: 'More', link: '/more/links/' },
       { text: 'Sponsor', link: 'https://github.com/sponsors/lupas' }
     ]
   }
---
+++ packages/docs/more/links/README.md
@@ -0,0 +1,9 @@
+# Links
+
+Collection of Links related to Nuxt-fire:
+
+- [Nuxt.js (v2), Firestore & SSR 🔥](https://medium.com/@pascalluther/nuxt-js-v2-firestore-ssr-938d8fb7d2b0?)  
+  Medium Article about integrating Firestore with Nuxt.js v2.
+
+- [Nuxt.js (v1), Firestore & SSR 🔥](https://medium.com/@pascalluther/nuxt-js-v1-firestore-and-ssr-73e3140574fc?)  
+  Medium Article about integrating Firestore with Nuxt.js v1.
