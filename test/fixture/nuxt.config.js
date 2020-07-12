4e96d28a43014c85a01a0c4c953f756bd2ec0812
pimlie <pimlie@hotmail.com>
2020-07-12 16:31:58 +0200

    feat: add support for lazy service loading
    refactor: prefer single injection
    
    feat: add option to disable injecting module
    
    chore: add tests

---
+++ test/fixture/nuxt.config.js
@@ -0,0 +1,31 @@
+import FirebaseModule from '../..'
+
+export default {
+  rootDir: __dirname,
+  render: {
+    resourceHints: false
+  },
+  modules: [
+    FirebaseModule
+  ],
+  firebase: {
+    lazy: true,
+    config: {
+      // REQUIRED: Official config for firebase.initializeApp(config):
+      apiKey: '<apiKey>',
+      authDomain: '<authDomain>',
+      databaseURL: 'https://test.firebaseio.com',
+      projectId: '<projectId>',
+      storageBucket: '<storageBucket>',
+      messagingSenderId: '<messagingSenderId>',
+      appId: '<appId>',
+      measurementId: '<measurementId>'
+    },
+    services: {
+      auth: true,
+      realtimeDb: true,
+      messaging: true,
+      analytics: true
+    }
+  }
+}
---
+++ test/fixture/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Works!</h1>
+</template>
+
+<script>
+export default {
+  async mounted () {
+    const app = await this.$fire.appReady()
+    console.log('app ready', app) // eslint-disable-line no-console
+  }
+}
+</script>
