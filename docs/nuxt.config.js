2523f2dd3c30a91f13f39d8555fecc73c35dfb40
Sébastien Chopin <seb@nuxtjs.com>
2020-09-24 13:35:50 +0200

    docs: add ackee tracking

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,3 +1,14 @@
 import theme from '@nuxt/content-theme-docs'
 
-export default theme({})
+export default theme({
+  loading: { color: '#FFA000' },
+  buildModules: [
+    // https://github.com/bdrtsky/nuxt-ackee
+    'nuxt-ackee'
+  ],
+  ackee: {
+    server: 'https://ackee.nuxtjs.com',
+    domainId: '6abbd929-404a-41f4-b647-fae6f4f8131b',
+    detailed: true
+  }
+})
