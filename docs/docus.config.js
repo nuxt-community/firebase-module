f1aee079fe44bca870813931f3b9212adc6eb6f4
Pascal Luther <pascal@luther.ch>
2021-06-11 22:52:35 +0200

    docs: Updated to never version of docus

---
+++ docs/docus.config.js
@@ -0,0 +1,24 @@
+export default {
+  title: 'Nuxt Firebase',
+  twitter: 'nuxt_js',
+  url: 'https://firebase.nuxtjs.org',
+  theme: {
+    header: {
+      logo: {
+        dark: '/logo-dark.svg',
+        light: '/logo-light.svg',
+      },
+      title: false,
+    },
+  },
+  github: {
+    repo: 'nuxt-community/firebase-module',
+    branch: 'dev',
+    dir: 'allDocuments',
+  },
+  algolia: {
+    apiKey: '26073a7590724b3efc377b3b7608f3e0',
+    indexName: 'nuxtjs_firebase',
+    langAttribute: 'lang',
+  },
+}
--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,19 +1,5 @@
 import { withDocus } from 'docus'
 
 export default withDocus({
-  docus: {
-    colors: {
-      primary: '#FFA000',
-    },
-  },
-  loading: { color: '#FFA000' },
-  buildModules: [
-    // https://github.com/bdrtsky/nuxt-ackee
-    'nuxt-ackee',
-  ],
-  ackee: {
-    server: 'https://ackee.nuxtjs.com',
-    domainId: '6abbd929-404a-41f4-b647-fae6f4f8131b',
-    detailed: true,
-  },
+  rootDir: __dirname,
 })
